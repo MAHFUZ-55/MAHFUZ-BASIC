652ae5dfc9b7c02afa8f7372e08f04fe65c22398
MAHFUZ-55 <127038327+MAHFUZ-55@users.noreply.github.com>
2023-04-12 09:15:30 +0600

    Add files via upload

---
+++ MAHFUZ-BASIC.py
@@ -0,0 +1,238 @@
+import os
+
+import sys
+
+os.system("clear")
+
+os.system("xdg-open https://www.facebook.com/mdmahfujhasan.joy")
+
+BLACK=("""
+
+\033[1;34m
+
+ 
+.------..------..------..------..------..------.
+|M.--. ||A.--. ||H.--. ||F.--. ||U.--. ||Z.--. |
+| (\/) || (\/) || :/\: || :(): || (\/) || :(): |
+| :\/: || :\/: || (__) || ()() || :\/: || ()() |
+| '--'M|| '--'A|| '--'H|| '--'F|| '--'U|| '--'Z|
+`------'`------'`------'`------'`------'`------'
+
+  
+
+\033[1;33m××××××××××××××××××××××××××××××××××××××××××××××
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m CREATED BY\033[1;33m     ☞  \033[1;32mMAHFUZ      \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m FACEBOK\033[1;33m        ☞  \033[1;32mMd Mahfuz Hasan  \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m GITHUB\033[1;33m         ☞  \033[1;32MAHFUZ-55   \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TEAM\033[1;33m           ☞  \033[1;32mJunior Cyber 71-We Protect You        \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS NAME\033[1;33m     ☞  \033[1;32mPKG INSTALL      \033[1;33m  |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS VIRSION\033[1;33m  ☞  \033[1;32m0.5         \033[1;33m   |
+
+|  \033[1;37m \033[1;31m[\033[1;32m✓\033[1;31m]\033[1;36m TOOLS STATUS\033[1;33m   ☞  \033[1;32mWORKING         \033[1;33m   |
+
+××××××××××××××××××××××××××××××××××××××××××××××
+
+""")
+
+print(BLACK)
+
+p=input("ARE YOU RAN THIS TOOL (y/n)  : ")
+
+p=p.replace(" ","")
+
+ys=["y","Y"]
+
+if p in ys:
+
+  print("TOOL IS RUNNING.....")
+
+elif p=="n":
+
+  print("STOP TOOL, PLEASE TRY AGAIN")
+
+  sys.exit()
+
+else:
+
+  print("YOU CHOICE IS WORNG")
+
+  sys.exit()
+
+print("\033[1;32m")
+
+os.system("pkg update -y")
+
+print("\033[1;32m")
+
+os.system("pkg upgrade -y")
+
+print("\033[1;32m")
+
+os.system("pkg install git -y")
+
+print("\033[1;32m")
+
+os.system("pkg install python -y")
+
+print("\033[1;32m")
+
+os.system("pkg install python2 -y")
+
+print("\033[1;32m")
+
+os.system("pkg install wget -y")
+
+print("\033[1;32m")
+
+os.system("pkg install figlet -y")
+
+print("\033[1;32m")
+
+os.system("pkg install curl -y")
+
+print("\033[1;32m")
+
+os.system("pkg install php -y")
+
+print("\033[1;32m")
+
+os.system("pkg install cmatrix -y")
+
+print("\033[1;32m")
+
+os.system("pkg install sl -y")
+
+print("\033[1;32m")
+
+os.system("pkg install ruby -y")
+
+print("\033[1;32m")
+
+os.system("gem install lolcat -y")
+
+print("\033[1;32m")
+
+os.system("pkg install zip -y")
+
+print("\033[1;32m")
+
+os.system("pkg install unzip -y")
+
+print("\033[1;32m")
+
+os.system("pkg install openssh -y")
+
+print("\033[1;32m")
+
+os.system("pkg install clang -y")
+
+print("\033[1;32m")
+
+os.system("pkg install crunch -y")
+
+print("\033[1;32m")
+
+os.system("pkg install proot-distro -y")
+
+print("\033[1;32m")
+
+os.system("pkg install ncurses-utils -y")
+
+print("\033[1;32m")
+
+os.system("python -m pip install --upgrade pip")
+
+print("\033[1;32m")
+
+os.system("python2 -m pip install --upgrade pip")
+
+print("\033[1;32m")
+
+os.system("pip install requests")
+
+print("\033[1;32m")
+
+os.system("pip2 install requests")
+
+print("\033[1;32m")
+
+os.system("pip install rich")
+
+print("\033[1;32m")
+
+os.system("pip2 install rich")
+
+print("\033[1;32m")
+
+os.system("pip install bs4")
+
+print("\033[1;32m")
+
+os.system("pip2 install bs4")
+
+print("\033[1;32m")
+
+os.system("pip install mechanize")
+
+print("\033[1;32m")
+
+os.system("pip2 install mechanize")
+
+print("""
+
+═══════════════════════════════════════════════════
+
+                ✅CONGRATULATION✅
+
+═══════════════════════════════════════════════════
+
+\033[1;33m
+
+                       /$$                        
+
+                      | $$                        
+
+  /$$$$$$$  /$$$$$$  /$$$$$$   /$$   /$$  /$$$$$$ 
+
+ /$$_____/ /$$__  $$|_  $$_/  | $$  | $$ /$$__  $$
+
+|  $$$$$$ | $$$$$$$$  | $$    | $$  | $$| $$  \ $$
+
+ \____  $$| $$_____/  | $$ /$$| $$  | $$| $$  | $$
+
+ /$$$$$$$/|  $$$$$$$  |  $$$$/|  $$$$$$/| $$$$$$$/
+
+|_______/  \_______/   \___/   \______/ | $$____/ 
+
+                                        | $$      
+
+                                        | $$      
+
+                                        |__/ 
+
+\033[1;32m
+
+    /$$$$$$$   /$$$$$$  /$$   /$$ /$$$$$$$$
+
+    | $$__  $$ /$$__  $$| $$$ | $$| $$_____/
+
+    | $$  \ $$| $$  \ $$| $$$$| $$| $$      
+
+    | $$  | $$| $$  | $$| $$ $$ $$| $$$$$   
+
+    | $$  | $$| $$  | $$| $$  $$$$| $$__/   
+
+    | $$  | $$| $$  | $$| $$\  $$$| $$      
+
+    | $$$$$$$/|  $$$$$$/| $$ \  $$| $$$$$$$$
+
+    |_______/  \______/ |__/  \__/|________/
+
+""")
+
